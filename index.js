3753da9eacc7bd46aaccf9785698b6ecdd9beb26
“folakemi007git config --global user.email “flakkyj45@gmail.com <flakkyj46@gmail.com>
2023-03-21 18:22:04 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let todoList = [
+    " Do devotion";
+    "Do house chores";
+    " prepare kids for school";
+    ""
+]
+
+function addItem (item) {
+
+}
+
+function editItem (item) {
+
+}
+
+function deleteItem (item) {
+
+}
